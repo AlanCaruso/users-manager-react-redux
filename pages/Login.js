5e44c64931896a6ddd3f1a9c01d8210409242962
Alan Caruso <alan.carusoo@gmail.com>
2023-05-10 20:46:54 -0300

    adding login

---
+++ pages/Login.js
@@ -0,0 +1,126 @@
+"use client";
+import React, { useState } from "react";
+import styled from "styled-components";
+import * as Yup from "yup";
+import axios from "axios";
+
+const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+`;
+
+const LoginHeader = styled.h2`
+  margin-bottom: 20px;
+`;
+
+const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 300px;
+`;
+
+const LoginInput = styled.input`
+  padding: 10px;
+  margin: 10px 0;
+  border: 2px solid #ccc;
+  border-radius: 5px;
+  width: 100%;
+`;
+
+const LoginButton = styled.button`
+  padding: 10px;
+  margin-top: 20px;
+  background-color: #0077cc;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  &:hover {
+    background-color: #005fa3;
+  }
+`;
+
+const LoginError = styled.div`
+  color: red;
+  font-size: 12px;
+  margin-top: 5px;
+`;
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: Yup.string().required("Password is required"),
+});
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    validationSchema
+      .validate({ email, password }, { abortEarly: false })
+      .then(() => {
+        axios
+          .post("https://reqres.in/api/login", {
+            email,
+            password,
+          })
+          .then((response) => {
+            console.log(response);
+            alert(`Logged in as ${response.data.email}`);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      })
+      .catch((err) => {
+        const newErrors = {};
+        err.inner.forEach((error) => {
+          newErrors[error.path] = error.message;
+        });
+        setErrors(newErrors);
+      });
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  return (
+    <LoginContainer>
+      <LoginHeader>Login</LoginHeader>
+      <LoginForm onSubmit={handleLogin}>
+        <LoginInput
+          type="text"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={handleInputChange}
+        />
+        {errors.email && <LoginError>{errors.email}</LoginError>}
+        <LoginInput
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={handleInputChange}
+        />
+        {errors.password && <LoginError>{errors.password}</LoginError>}
+        <LoginButton type="submit">Login</LoginButton>
+      </LoginForm>
+    </LoginContainer>
+  );
+};
+
+export default Login;
